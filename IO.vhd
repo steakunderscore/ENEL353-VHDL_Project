9b14a3d49fe8e7e91fce436a3bf8269a9169faf0
Henry Jenkins <henry@henryjenkins.name>
2010-09-11 06:38:12 +0000

    Added some base packages and modules
    git-svn-id: https://svn.elec.canterbury.ac.nz/groups/enel353g1@2 ab56b3d6-988e-0410-8987-bf0f3d9577cd

---
+++ IO.vhd
@@ -0,0 +1,39 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    16:08:33 09/15/2010 
+-- Design Name: 
+-- Module Name:    IO - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity IO is
+end IO;
+
+architecture Behavioral of IO is
+
+begin
+
+
+end Behavioral;
+
---
+++ cpu.vhd
@@ -0,0 +1,39 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    16:09:46 09/15/2010 
+-- Design Name: 
+-- Module Name:    cpu - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity cpu is
+end cpu;
+
+architecture Behavioral of cpu is
+
+begin
+
+
+end Behavioral;
+
