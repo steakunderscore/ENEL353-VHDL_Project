bb1f2634a7f0621138b7d5a736a04faa075da784
Henry Jenkins <henry@henryjenkins.name>
2010-09-12 04:33:18 +0000

    Added some registers and buses to the project
    git-svn-id: https://svn.elec.canterbury.ac.nz/groups/enel353g1@3 ab56b3d6-988e-0410-8987-bf0f3d9577cd

--- Buses.vhd
+++ Buses.vhd
@@ -1,61 +1,36 @@
---	Package File Template
+--	Package To Define The Bus Data Types
 --
---	Purpose: This package defines supplemental types, subtypes, 
---		 constants, and functions 
+--	Purpose: This package is here to keep all the data types 
+-- standard accross the whole project
+--
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 
-package busses is
+package buses is
 
   
-  type <new_type> is
+  type instruction is
     record
-        <type_name>        : std_logic_vector( 7 downto 0);
-        <type_name>        : std_logic;
+        opcode             : std_logic_vector( 5 downto 0);
+		  operand            : std_logic_vector( 9 downto 0);
     end record;
 
 -- Declare constants
 
-  constant <constant_name>		: time := <time_unit> ns;
-  constant <constant_name>		: integer := <value>;
- 
+  constant instructionLength  : integer := 16;
+  constant opcodeLength       : integer := 6;
+  constant operandLength      : integer := 10;
+   
 -- Declare functions and procedure
 
-  function <function_name>  (signal <signal_name> : in <type_ declaration>) return <type_declaration>;
-  procedure <procedure_name>	(<type_declaration> <constant_name>	: in <type_declaration>);
+  --function <function_name>  (signal <signal_name> : in <type_ declaration>) return <type_declaration>;
+  --procedure <procedure_name>	(<type_declaration> <constant_name>	: in <type_declaration>);
 
-end busses;
-
-
-package body busses is
-
--- Example 1
-  function <function_name>  (signal <signal_name> : in <type_declaration>  ) return <type_declaration> is
-    variable <variable_name>     : <type_declaration>;
-  begin
-    <variable_name> := <signal_name> xor <signal_name>;
-    return <variable_name>; 
-  end <function_name>;
+end buses;
 
 
--- Example 2
-  function <function_name>  (signal <signal_name> : in <type_declaration>;
-                         signal <signal_name>   : in <type_declaration>  ) return <type_declaration> is
-  begin
-    if (<signal_name> = '1') then
-      return <signal_name>;
-    else
-      return 'Z';
-    end if;
-  end <function_name>;
-
--- Procedure Example
-  procedure <procedure_name>  (<type_declaration> <constant_name>  : in <type_declaration>) is
-    
-  begin
-    
-  end <procedure_name>;
- 
-end busses;
+package body buses is
+  -- Is emtpy
+end buses;
--- cpu.vhd
+++ cpu.vhd
@@ -28,12 +28,59 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 --use UNISIM.VComponents.all;
 
 entity cpu is
+
+  PORT(
+    clk            : IN std_logic;
+    Rinsel         : IN std_logic_vector(2 DOWNTO 0); -- Which register to write
+	 Routsel        : IN std_logic_vector(2 DOWNTO 0); -- Which register to read
+    read, write    : IN std_logic; -- Flags for read or write control
+    Rin            : IN std_logic_vector(7 DOWNTO 0); -- Input signals
+    Rout           : OUT std_logic_vector(7 DOWNTO 0)); -- Output signals
+
 end cpu;
 
 architecture Behavioral of cpu is
 
+SIGNAL reg0, reg1, reg2, reg3, reg4, reg5, reg6, reg7 : std_logic_vector(7 DOWNTO 0);
+
 begin
 
+  -- WRITE PROCESS: To write to the registers
+  PROCESS (clk, write, Rinsel)
+  BEGIN
+    IF clk 'EVENT AND clk= '1' THEN
+      IF write = '1' THEN
+        CASE Rinsel IS
+          WHEN "000" => reg0 <= Rin;
+          WHEN "001" => reg1 <= Rin;
+          WHEN "010" => reg2 <= Rin;
+          WHEN "011" => reg3 <= Rin;
+          WHEN "100" => reg4 <= Rin;
+          WHEN "101" => reg5 <= Rin;
+          WHEN "110" => reg6 <= Rin;
+          WHEN "111" => reg7 <= Rin;
+          WHEN OTHERS => NULL;
+        END CASE;
+      END IF;
+    END IF;
+  END PROCESS;
+
+  -- READ PROCESS: To read from the registers
+  PROCESS (read, Routsel)
+  BEGIN
+    IF read = '1' THEN
+      CASE Routsel IS
+        WHEN "000" => Rout <= reg0;
+        WHEN "001" => Rout <= reg1;
+        WHEN "010" => Rout <= reg2;
+        WHEN "011" => Rout <= reg3;
+        WHEN "100" => Rout <= reg4;
+        WHEN "101" => Rout <= reg5;
+        WHEN "110" => Rout <= reg6;
+        WHEN "111" => Rout <= reg7;
+        --WHEN OTHERS => Rout <= "XXXXXXXXXXXXXXXXXXXXXX";
+      END CASE;
+    END IF;
+  END PROCESS;
 
 end Behavioral;
-
