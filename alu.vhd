bb5c2e7ddea79ef274732967c5b239be8df67a24
Henry Jenkins <henry@henryjenkins.name>
2010-09-16 00:33:58 +0000

    Added the status register code and broke apart the opcode into case statments
    git-svn-id: https://svn.elec.canterbury.ac.nz/groups/enel353g1@5 ab56b3d6-988e-0410-8987-bf0f3d9577cd

--- alu.vhd
+++ alu.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date:    18:59:20 09/18/2010 
+-- Create Date:  18:59:20 09/18/2010 
 -- Design Name: 
--- Module Name:    alu - Behavioral 
+-- Module Name:  alu - Behavioral 
 -- Project Name: 
 -- Target Devices: 
 -- Tool versions: 
@@ -30,55 +30,77 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity alu is
-    Port ( opcode : in   STD_LOGIC_VECTOR (3 downto 0);
-           rx     : in   STD_LOGIC_VECTOR (7 downto 0);
-           ry     : in   STD_LOGIC_VECTOR (7 downto 0);
-           ro     : out  STD_LOGIC_VECTOR (7 downto 0);
-           Z      : out  STD_LOGIC;
-           Cin    : in   STD_LOGIC; -- Caarry in
-           Cout   : out  STD_LOGIC; -- Carry out
-           N      : out  STD_LOGIC);
+   Port (clk : in   STD_LOGIC;                      -- Clock
+         f   : in   STD_LOGIC_VECTOR (3 downto 0);  -- Function (opcode)
+         rx  : in   STD_LOGIC_VECTOR (7 downto 0);  -- Input x (Rx)
+         ry  : in   STD_LOGIC_VECTOR (7 downto 0);  -- Input y (Ry)
+         ro  : out  STD_LOGIC_VECTOR (7 downto 0);  -- Output Normaly (Ry)
+         Cin : in   STD_LOGIC;                      -- Carry in
+         sr  : out  STD_LOGIC_VECTOR (2 downto 0)); -- Status register out {Z,C,N}
 end alu;
 
 architecture Behavioral of alu is
 
 begin
-    process(Cin, ry, rx, opcode)
-    begin
-    
-    -- use case statement to achieve 
-    -- different operations of ALU
+  process(clk, f, rx, ry, Cin)
+  begin
+  
+  -- use case statement to achieve 
+  -- different operations of ALU
 
-    case opcode is
-        when "0001" => -- Do AND operation
-				ro <= ry and rx;
-        when "0011" => -- Do OR  operation
-		      ro <= ry or rx;
-        when "0101" => -- Do NOT operation
-		      ro <= not rx;
-        when "0111" => -- Do XOR operation
-		      ro <= ry xor rx;
-        when "1001" => -- Do ADD operation
-		      ro <= ry + rx;
-        when "1011" => -- Do ADC operation
-		      --ro <= ry + rx + Cin;
-        when "1101" => -- Do SUB operation
-		      ro <= ry - rx;
-        when "1111" => -- Do SBB operation
-		      --ro <= (ry - rx - Cin);
-        when "0100" => -- Do NEG operation ( two's complement )
-		      --ro <= ((not rx) + 1);
-        when "0110" => -- Do CMP operation
-		      if ( ry > rx ) then
-				    ro <= ry;
-		      else
-					 ro <= rx;
-			   end if;
-        when others =>
-            ro <= "XXXXXXXX";
+  if (clk='1' and clk'event) then
+    case f(0) is
+      when '1' => -- '1' means that it is a arithmetic or logic function
+        case f(3 downto 1) is
+          case f(3) is
+            
+            when '0' => -- It is a logic function
+              case f(2 downto 1) is
+                when "00" => -- Do AND operation
+                  ro <= ry and rx;
+                when "01" => -- Do OR  operation
+                  ro <= ry or rx;
+                when "10" => -- Do NOT operation
+                  ro <= not rx;
+                when "11" => -- Do XOR operation
+                  ro <= ry xor rx;
+              end case;
+              if (ro = "00000000") then -- Set the status register
+                sr(0) <= '0';
+              end if;
+              sr(1) <= '0';
+              sr(2) <= ro(7); -- This might need to be changed to '0'
+
+            when '1' => -- It is a arithmetic function
+              case f(2 downto 1) is
+                when "00" => -- Do ADD operation
+                  ro <= ry + rx;
+                when "01" => -- Do ADC operation
+                  ro <= ry + rx + Cin;
+                when "10" => -- Do SUB operation
+                  ro <= ry - rx;
+                when "11" => -- Do SBB operation
+                  ro <= (ry - rx - Cin);
+              end case;
+
+        end case;
+      when '0' => -- the other functions
+        case f(3 downto 1) is
+          when "010" => -- Do NEG operation ( two's complement )
+            ro <= ((not rx) + 1);
+          when "011" => -- Do CMP operation
+            if ( ry > rx ) then
+              ro <= ry;
+            else
+              ro <= rx;
+            end if;
+        end case;
+      when others =>
+        ro <= "XXXXXXXX";
     end case;
+  end if;
 
-    end process;
+  end process;
 
 end Behavioral;
 
