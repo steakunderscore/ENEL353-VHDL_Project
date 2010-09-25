51b5afc896bf74aff73c0b0d4e4ea84a98d1f719
Henry Jenkins <henry@henryjenkins.name>
2010-09-25 01:24:52 +0000

    Started testing the alu code and wrote some test code in testRoutines.vhd
    git-svn-id: https://svn.elec.canterbury.ac.nz/groups/enel353g1@7 ab56b3d6-988e-0410-8987-bf0f3d9577cd

--- alu.vhd
+++ alu.vhd
@@ -19,6 +19,13 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+
+library work;
+use work.cpuLib.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -36,77 +43,95 @@ entity alu is
          ry  : in   STD_LOGIC_VECTOR (7 downto 0);  -- Input y (Ry)
          ro  : out  STD_LOGIC_VECTOR (7 downto 0);  -- Output Normaly (Ry)
          Cin : in   STD_LOGIC;                      -- Carry in
-         sr  : out  STD_LOGIC_VECTOR (2 downto 0)); -- Status register out {Z,C,N}
+         sr  : out  STD_LOGIC_VECTOR (2 downto 0)); -- Status register out Z(0), C(1), N(2)
 end alu;
 
-architecture Behavioral of alu is
 
-begin
-  process(clk, f, rx, ry, Cin)
-  begin
-  
+architecture Behavioral of alu is
+  signal   temp   : std_logic_vector(8 downto 0); -- Used so addition with carry can occour
+BEGIN
+  process(f, rx, ry, Cin)
+    variable output : std_logic_vector(7 downto 0); -- used to allow reading of ro
+	 variable Z,C,N  : std_logic; -- Make the code easier to read
+  BEGIN
   -- use case statement to achieve 
   -- different operations of ALU
 
-  if (clk='1' and clk'event) then
     case f(0) is
       when '1' => -- '1' means that it is a arithmetic or logic function
-        case f(3 downto 1) is
-          case f(3) is
-            
-            when '0' => -- It is a logic function
-              case f(2 downto 1) is
-                when "00" => -- Do AND operation
-                  ro <= ry and rx;
-                when "01" => -- Do OR  operation
-                  ro <= ry or rx;
-                when "10" => -- Do NOT operation
-                  ro <= not rx;
-                when "11" => -- Do XOR operation
-                  ro <= ry xor rx;
-              end case;
-              if (ro = "00000000") then -- Set the status register
-                sr(0) <= '0';
-              end if;
-              sr(1) <= '0';
-              sr(2) <= ro(7); -- This might need to be changed to '0'
-
-            when '1' => -- It is a arithmetic function
-              case f(2 downto 1) is
-                when "00" => -- Do ADD operation
-                  ro <= ry + rx;
-                when "01" => -- Do ADC operation
-                  ro <= ry + rx + Cin;
-                when "10" => -- Do SUB operation
-                  ro <= ry - rx;
-                when "11" => -- Do SBB operation
-                  ro <= (ry - rx - Cin);
-              end case;
-
-        end case;
-      when '0' => -- the other functions
-        case f(3 downto 1) is
-          when "010" => -- Do NEG operation ( two's complement )
-            ro <= ((not rx) + 1);
-            -- TODO: Add the status register output
-          when "011" => -- Do CMP operation
-            if ( ry > rx ) then
-              ro <= ry;
-            else
-              ro <= rx;
+        case f(3) is --seperate the arithmetic and logic function
+          when '0' => -- It is a logic function
+            case f(2 downto 1) is
+              when "00" => -- Do AND operation
+                output := ry and rx;
+              when "01" => -- Do OR  operation
+                output := ry or rx;
+              when "10" => -- Do NOT operation
+                output := not rx;
+              when "11" => -- Do XOR operation
+                output := ry xor rx;
+				  when others =>
+				    output := (others => '0');
+            end case;
+            if (output = "00000000") then -- Set the Zero in status register
+              Z := '1';
+				ELSE
+				  Z := '0';
             end if;
-            if (ro = "00000000") then
-              sr(0) <= '0';
-              sr(1) <= '0';
-              sr(2) <= ro(7); -- This might need to be changed to '0'
+            C := '0'; -- Carry is always 0
+            N := output(7); -- This might need to be changed to '0'
+
+          when '1' => -- It is a arithmetic function
+            case f(2 downto 1) is
+              when "00" => -- Do ADD operation
+                temp <= ( '0' & ry ) + ( '0' & rx );
+              when "01" => -- Do ADC operation
+                temp <= ( '0' & ry ) + ( '0' & rx ) + ( "00000000" & Cin ); --TODO: check this logic
+              when "10" => -- Do SUB operation
+                temp <= ry - rx;
+              when "11" => -- Do SBB operation
+                temp <= ( '0' & ry ) - ( '0' & rx ) - ( "00000000" & Cin ); --TODO: check this logic
+              when others =>
+		          output := (others => '0');
+            end case;
+				output := temp(7 downto 0);
+            if (output = "00000000") then -- Set the Zero in status register
+              Z := '1';
+			   ELSE
+				  Z := '0';
             end if;
+				C := temp(8);
+				N := temp(7);
+          when others =>
+		      output := (others => '0');
         end case;
+--		when '0' => -- Non arithmetic or locic
+--        case f(3 downto 1) is
+--          when "010" => -- Do NEG operation ( two's complement )
+--            output <= ((not rx) + 1);
+--            -- TODO: Add the status register output
+--          when "011" => -- Do CMP operation
+--            if ( ry > rx ) then
+--              output <= ry;
+--            else
+--              output <= rx;
+--            end if;
+----            if (output = "00000000") then
+----              Z := '0';
+----              C := '0';
+----              N := output(7); -- This might need to be changed to '0'
+----            end if;
+--          when others =>
+--			   output => (others => '0');
+--        end case;
       when others =>
-        ro <= "XXXXXXXX";
-    end case;
-  end if;
+        output := (others => '0');
+    end case;  --  f(0)
+	 ro <= output;
+	 sr(0) <= Z; --Z(0)
+	 sr(1) <= C; --C(1)
+    sr(2) <= N; --N(2)
 
   end process;
-
 end Behavioral;
 
---
+++ testRoutines.vhd
@@ -0,0 +1,276 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   20:29:18 09/28/2010
+-- Design Name:   
+-- Module Name:   C:/Users/Henry/Desktop/enel353g1/testRoutines.vhd
+-- Project Name:  enel353g1
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: alu
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY testRoutines IS
+END testRoutines;
+ 
+ARCHITECTURE behavior OF testRoutines IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT alu
+    PORT(
+         clk : IN  std_logic;
+         f : IN  std_logic_vector(3 downto 0);
+         rx : IN  std_logic_vector(7 downto 0);
+         ry : IN  std_logic_vector(7 downto 0);
+         ro : OUT  std_logic_vector(7 downto 0);
+         Cin : IN  std_logic;
+         sr : OUT  std_logic_vector(2 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal f : std_logic_vector(3 downto 0) := (others => '0');
+   signal rx : std_logic_vector(7 downto 0) := (others => '0');
+   signal ry : std_logic_vector(7 downto 0) := (others => '0');
+   signal Cin : std_logic := '0';
+
+ 	--Outputs
+   signal ro : std_logic_vector(7 downto 0);
+   signal sr : std_logic_vector(2 downto 0);
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: alu PORT MAP (
+          clk => clk,
+          f => f,
+          rx => rx,
+          ry => ry,
+          ro => ro,
+          Cin => Cin,
+          sr => sr
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 10 ns;	
+
+      wait for clk_period*2;
+
+      -- insert stimulus here
+		f <= "0001";       -- Test AND Function
+		rx <= "01010101";
+		ry <= "10101010";
+		wait for 2 ns;
+		
+		rx <= "00001000";
+		ry <= "00000000";
+		wait for 2 ns;
+		
+		rx <= "11111111";
+		ry <= "00000000";
+		wait for 2 ns;
+		
+		rx <= "00000000";
+		ry <= "11111111";
+		wait for 2 ns;
+				
+		rx <= "11111111";
+		ry <= "11111111";
+		wait for 2 ns;
+
+		rx <= "11111111";
+		ry <= "11111111";
+		wait for 2 ns;
+
+		rx <= "10101010";
+		ry <= "10101010";
+		wait for 2 ns;
+		
+		rx <= "01010101";
+		ry <= "01010101";
+		wait for 2 ns;
+		
+		f <= "0011";       -- Test OR Function
+		rx <= "01010101";
+		ry <= "10101010";
+		wait for 2 ns;
+		
+		rx <= "00001000";
+		ry <= "00000000";
+		wait for 2 ns;
+		
+		rx <= "11111111";
+		ry <= "00000000";
+		wait for 2 ns;
+		
+		rx <= "00000000";
+		ry <= "11111111";
+		wait for 2 ns;
+				
+		rx <= "11111111";
+		ry <= "11111111";
+		wait for 2 ns;
+
+		rx <= "11111111";
+		ry <= "11111111";
+		wait for 2 ns;
+
+		rx <= "10101010";
+		ry <= "10101010";
+		wait for 2 ns;
+		
+		rx <= "01010101";
+		ry <= "01010101";
+		wait for 2 ns;
+		
+		f <= "0101";       -- Test NOT Function
+		rx <= "01010101";
+		ry <= "10101010";
+		wait for 2 ns;
+		
+		rx <= "00001000";
+		ry <= "00000000";
+		wait for 2 ns;
+		
+		rx <= "11111111";
+		ry <= "00000000";
+		wait for 2 ns;
+		
+		rx <= "00000000";
+		ry <= "11111111";
+		wait for 2 ns;
+				
+		rx <= "11111111";
+		ry <= "11111111";
+		wait for 2 ns;
+
+		rx <= "11111111";
+		ry <= "11111111";
+		wait for 2 ns;
+
+		rx <= "10101010";
+		ry <= "10101010";
+		wait for 2 ns;
+		
+		rx <= "01010101";
+		ry <= "01010101";
+		wait for 2 ns;
+		
+		f <= "0111";       -- Test XOR Function
+		rx <= "01010101";
+		ry <= "10101010";
+		wait for 2 ns;
+		
+		rx <= "00001000";
+		ry <= "00000000";
+		wait for 2 ns;
+		
+		rx <= "11111111";
+		ry <= "00000000";
+		wait for 2 ns;
+		
+		rx <= "00000000";
+		ry <= "11111111";
+		wait for 2 ns;
+				
+		rx <= "11111111";
+		ry <= "11111111";
+		wait for 2 ns;
+
+		rx <= "11111111";
+		ry <= "11111111";
+		wait for 2 ns;
+
+		rx <= "10101010";
+		ry <= "10101010";
+		wait for 2 ns;
+		
+		rx <= "01010101";
+		ry <= "01010101";
+		wait for 2 ns;
+
+		
+
+      wait;
+   end process;
+--
+--	process
+--     type pattern_type is record
+--	    --  The inputs of the adder.
+--       f : bit_vector(3 downto 0);
+--		 rx : bit_vector(7 downto 0);
+--		 ry : bit_vector(7 downto 0);
+--       Cin : bit;
+--       --  The expected outputs of the adder.
+--		 ro : bit_vector(7 downto 0);
+--       sr : bit_vector(2 downto 0);
+--     end record;
+--     --  The patterns to apply.
+--     type pattern_array is array (natural range <>) of pattern_type;
+--       constant patterns : pattern_array :=
+--       --(( f    ,  rx       ,  ry       , Cin,  ro,         sr  ),
+--			(("0001", "00000000", "00000000", '0', "00000000", "000"),
+--          ("0001", "00000000", "00000000", '0', "00000000", "000"),
+--          ("0001", "00000000", "00000000", '0', "00000000", "000"));
+--     begin
+--     --  Check each pattern.
+--       for i in patterns'range loop
+--          --  Set the inputs.
+--          f   <= patterns(i).f;
+--          rx  <= patterns(i).rx;
+--          ry  <= patterns(i).ry;
+--			 Cin <= patterns(i).Cin;
+--          --  Wait for the results.
+--          wait for 1 ns;
+--          --  Check the outputs.
+--          assert ro = patterns(i).ro
+--             report "bad sum value" severity error;
+--          assert sr = patterns(i).sr
+--             report "bad carray out value" severity error;
+--       end loop;
+--       assert false report "end of test" severity note;
+--       --  Wait forever; this will finish the simulation.
+--       wait;
+--    end process;
+END;
