0830837fee7d338f0ccf363babe15c3c0d1c9851
Joel Koh <jmk35@uclive.ac.nz>
2010-10-10 01:14:18 +0000

    updated gpr tests
    git-svn-id: https://svn.elec.canterbury.ac.nz/groups/enel353g1@35 ab56b3d6-988e-0410-8987-bf0f3d9577cd

--- processor/gpr_tb.vhd
+++ processor/gpr_tb.vhd
@@ -44,21 +44,21 @@ begin
 
     -- Read R0 from Rx
     SelRx <= "000";
-    wait for 10 ns;
+    wait for 20 ns;
     assert (Rx = "00010100") report "Read from Rx failed #1" severity error;
 
     -- Read R0 from Ry
     SelRy <= "000";
-    wait for 10 ns;
+    wait for 20 ns;
     assert (Ry = "00010100") report "Read from Ry failed #1" severity error;
 
     -- Disable write
     enable <= '0';
-    wait for 10 ns;
+    wait for 20 ns;
 
     -- Change Ri (should not write as it is disabled)
     Ri <= "00101010";
-    wait for 10 ns;
+    wait for 20 ns;
     assert (Rx = "00010100") report "Wrote to register while disabled #1" severity error;
     assert (Ry = "00010100") report "Wrote to register while disabled #2" severity error;
     
@@ -71,28 +71,39 @@ begin
     -- Write to R2
     SelRi <= "010";
     Ri <= "01010001";
-    wait for 10 ns;
+    wait for 20 ns;
 
     -- Read R2 from Rx
     SelRx <= "010";
-    wait for 10 ns;
+    wait for 20 ns;
     assert (Rx = "01010001") report "Read from Rx failed #3" severity error;
 
     -- Read R2 from Ry
     SelRy <= "010";
-    wait for 10 ns;
+    wait for 20 ns;
     assert (Ry = "01010001") report "Read from Ry failed #3" severity error;
 
     -- Read R0 from Rx again (should not have changed from previous results)
     SelRx <= "000";
-    wait for 10 ns;
+    wait for 20 ns;
     assert (Rx = "00101010") report "Read from Rx failed #4" severity error;
     
     -- Read R0 from Ry again (should not have changed from previous results)
     SelRy <= "000";
-    wait for 10 ns;
+    wait for 20 ns;
     assert (Ry = "00101010") report "Read from Ry failed #4" severity error;
 
+    -- Wait for a long time
+    wait for 1 ms;
+    assert (Rx = "00101010") report "Read from Rx failed #5" severity error;
+    assert (Ry = "00101010") report "Read from Ry failed #5" severity error;
+    
+    -- Read R2 after a long time
+    SelRx <= "010";
+    SelRy <= "010";
+    wait for 1 ms;
+    assert (Rx = "01010001") report "Read from Rx failed #6" severity error;
+    assert (Ry = "01010001") report "Read from Ry failed #6" severity error;
 
 
     assert false report "End of test" severity note;
