2e0968bccee6e5881b9f647cf0dacc50226e92ea
Henry Jenkins <henry@henryjenkins.name>
2010-10-13 08:04:39 +0000

    Removed dependency of using the + operator from ieee.numeric_std
    git-svn-id: https://svn.elec.canterbury.ac.nz/groups/enel353g1@68 ab56b3d6-988e-0410-8987-bf0f3d9577cd

--- processor/alu.vhd
+++ processor/alu.vhd
@@ -47,29 +47,21 @@ end alu;
 
 
 architecture alu_arch of alu is
-  component fulladder IS
-  Port (A    : in   STD_LOGIC;
-        B    : in   STD_LOGIC;
+  component fulladder8 IS
+  Port (A    : in   STD_LOGIC_VECTOR( 7 downto 0);
+        B    : in   STD_LOGIC_VECTOR( 7 downto 0);
         Cin  : in   STD_LOGIC;
-        Sum  : out  STD_LOGIC;
+        Sum  : out  STD_LOGIC_VECTOR( 7 downto 0);
         Cout : out  STD_LOGIC
         );
   end component;
-  signal   AdderCin  : std_logic;
   signal   A         : std_logic_vector(7 downto 0);
   signal   B         : std_logic_vector(7 downto 0);
-  signal   C         : std_logic_vector(6 downto 0);
+  signal   AdderCin  : std_logic;
   signal   Sum       : std_logic_vector(7 downto 0);
   signal   AdderCout : std_logic;
 BEGIN
-  Stage0: fulladder port map(A(0), B(0), AdderCin, Sum(0), C(0));
-  Stage1: fulladder port map(A(1), B(1), C(0), Sum(1), C(1));
-  Stage2: fulladder port map(A(2), B(2), C(1), Sum(2), C(2));
-  Stage3: fulladder port map(A(3), B(3), C(2), Sum(3), C(3));
-  Stage4: fulladder port map(A(4), B(4), C(3), Sum(4), C(4));
-  Stage5: fulladder port map(A(5), B(5), C(4), Sum(5), C(5));
-  Stage6: fulladder port map(A(6), B(6), C(5), Sum(6), C(6));
-  Stage7: fulladder port map(A(7), B(7), C(6), Sum(7), AdderCout);
+  Adder: fulladder8 port map(A, B, AdderCin, Sum, AdderCout);
   process(f, rx, ry, Cin)
     variable output : std_logic_vector(7 downto 0); -- used to allow reading of ro
     variable Z,C,N  : std_logic; -- Make the code easier to read
@@ -101,46 +93,38 @@ BEGIN
             C := '0'; -- Carry is always 0
             N := output(7); -- This might need to be changed to '0'
 
---          when '1' => -- It is a arithmetic function
---            case f(2 downto 1) is
---              when "00" => -- Do ADD operation
---                AdderCin <= '0';
---                A <= ry;
---                B <= rx;
---                output := Sum;
---                C := AdderCout;
---                N := output(7);
---              when "01" => -- Do ADC operation
---                AdderCin <= Cin;
---                A <= ry;
---                B <= rx;
---                output := Sum;
---                C := AdderCout;
---                N := output(7);
---              when "10" => -- Do SUB operation
---                AdderCin <= '1';
---                A <= ry;
---                B <= (not rx);
---                output := Sum;
---                C := AdderCout;
---                N := output(7);
---              when "11" => -- Do SBB operation
---                AdderCin <= (not Cin);
---                A <= ry;
---                B <= (not rx);
---                output := Sum;
---                C := AdderCout;
---                N := output(7);
---              when others =>
---                output := (others => '0');
---                C := AdderCout;
---                N := output(7);
---            end case;
---            if (output = "00000000") then -- Set the Zero in status register
---              Z := '1';
---            ELSE
---              Z := '0';
---            end if;
+          when '1' => -- It is a arithmetic function
+            case f(2 downto 1) is
+              when "00" => -- Do ADD operation
+                AdderCin <= '0';
+                A <= ry;
+                B <= rx;
+                output := Sum;
+              when "01" => -- Do ADC operation
+                AdderCin <= Cin;
+                A <= ry;
+                B <= rx;
+                output := Sum;
+              when "10" => -- Do SUB operation
+                AdderCin <= '1';
+                A <= ry;
+                B <= (not rx);
+                output := Sum;
+              when "11" => -- Do SBB operation
+                AdderCin <= (not Cin);
+                A <= ry;
+                B <= (not rx);
+                output := Sum;
+              when others =>
+                output := (others => '0');
+            end case;
+            if output = "00000000" THEN
+              Z := '1';
+            else
+              Z := '0';
+            END IF;
+            C := AdderCout;
+            N := output(7);
           when others =>
             output := (others => '0');
         end case;
@@ -148,17 +132,18 @@ BEGIN
         case f(3 downto 1) is
           when "010" => -- Do NEG operation ( two's complement )
             AdderCin <= '1';
---            A <= (others => '0');
+            A <= (others => '0');
             B <= (not rx);
             output := Sum;
             C := AdderCout;
             N := output(7);
           when "011" => -- Do CMP operation
---            AdderCin <= '1';
+            AdderCin <= '1';
             A <= rx;
---            B <= (not ry);
---            C := AdderCout;
---            N := output(7);
+            B <= (not ry);
+            output := Sum;
+            C := AdderCout;
+            N := output(7);
           when others =>
             output := (others => '0');
         end case;
