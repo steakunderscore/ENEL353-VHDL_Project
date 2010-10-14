22d22cd07ee97a40a2d52d5f5c2d4f5a4cee4102
Joel Koh <jmk35@uclive.ac.nz>
2010-10-14 06:43:14 +0000

    updates to control unit
    git-svn-id: https://svn.elec.canterbury.ac.nz/groups/enel353g1@73 ab56b3d6-988e-0410-8987-bf0f3d9577cd

--- processor/cu.vhd
+++ processor/cu.vhd
@@ -41,14 +41,19 @@ entity cu is
         alu_f       : out STD_LOGIC_VECTOR (3 downto 0);  -- Function
 
         -- General Purpose Registers
+        gpr_InSel   : out STD_LOGIC;                      -- select the input path (0 - cu, 1 - ALU)
         gpr_en      : out STD_LOGIC;                      -- enable write to GPR
         gpr_SelRx   : out STD_LOGIC_VECTOR (2 downto 0);  -- select GPR output x
         gpr_SelRy   : out STD_LOGIC_VECTOR (2 downto 0);  -- select GPR output y
         gpr_SelRi   : out STD_LOGIC_VECTOR (2 downto 0);  -- select GPR input
+        gpr_Ri      : out STD_LOGIC_VECTOR (7 downto 0); -- input to GPR
+        gpr_Rx      : out STD_LOGIC_VECTOR (7 downto 0); -- Rx to GPR
+        gpr_Ry      : out STD_LOGIC_VECTOR (7 downto 0); -- Ry to GPR
 
         -- Special Purpose Register
         spr_en      : out STD_LOGIC;                      -- enable write to SPR
-        spr_Ro      : in STD_LOGIC_VECTOR (15 downto 0);  -- Ro from SPR
+        spr_Ro      : in STD_LOGIC_VECTOR (15 downto 0);  -- output from SPR
+        -- control unit doesnt write to SPR, the ALU does
         
         -- Program Counter
         pc_en       : out STD_LOGIC;                      -- enable write to PC
@@ -58,23 +63,61 @@ entity cu is
         -- Address Registers
         ar_en       : out STD_LOGIC;                      -- enable write to AR
         ar_SelRi    : out STD_LOGIC_VECTOR (1 downto 0);  -- select AR in
-        ar_SelRo    : out STD_LOGIC_VECTOR (1 downto 0)   -- select AR out
+        ar_SelRo    : out STD_LOGIC_VECTOR (1 downto 0);  -- select AR out
+        ar_Ri       : out STD_LOGIC_VECTOR (15 downto 0); -- input to AR
+        ar_Ro       : in STD_LOGIC_VECTOR (15 downto 0)   -- output from AR
 
         );
 end cu;
 
 
 architecture Behavioral of cu is
+  component fulladder8 IS
+  Port (A    : in   STD_LOGIC_VECTOR(7 downto 0);
+        B    : in   STD_LOGIC_VECTOR(7 downto 0);
+        Cin  : in   STD_LOGIC;
+        Sum  : out  STD_LOGIC_VECTOR(7 downto 0);
+        Cout : out  STD_LOGIC
+        );
+  end component;
+  signal   A         : std_logic_vector(7 downto 0);
+  signal   B         : std_logic_vector(7 downto 0);
+  signal   AdderCin  : std_logic;
+  signal   Sum       : std_logic_vector(7 downto 0);
+  signal   AdderCout : std_logic;
+
+  signal v    : STD_LOGIC_VECTOR(7 downto 0);  -- 8-bit immediate
+
+  -- Makes code easier to read
+  signal rx : std_logic_vector(2 downto 0);
+  signal ry : std_logic_vector(2 downto 0);
+  signal ay : std_logic_vector(1 downto 0);
+
 BEGIN
+  Adder: fulladder8 port map(A, B, AdderCin, Sum, AdderCout);
 
   -- Process instruction
+  -- Assumes all instructions are valid
   process(opcode)
-    variable v    : STD_LOGIC_VECTOR (7 downto 0);  -- 8-bit immediate
-    variable temp : STD_LOGIC_VECTOR (7 downto 0);
   BEGIN
+
+    -- Disable writing to all registers at start
+    gpr_en <= '0';
+    spr_en <= '0';
+    pc_en <= '0';
+    ar_en <= '0';
 
     -- ALU
     if opcode(15) = '0' and opcode(10) = '0' and not opcode(14 downto 11) = "0010" then
+
+      ry <= opcode(7 downto 5);
+      rx <= opcode(2 downto 0);
+
+      gpr_SelRy <= ry;
+      gpr_SelRx <= rx;
+      gpr_SelRi <= ry;
+      gpr_InSel <= '1';
+      alu_f <= opcode(14 downto 11);
 
       if opcode(14 downto 11) = "0110" then -- CMP doesnt write to gpr
         gpr_en <= '0';
@@ -83,92 +126,171 @@ BEGIN
       end if;
 
       spr_en <= '1';
-      pc_en <= '0';
-      ar_en <= '0';
 
-      alu_f <= opcode(14 downto 11);
-      gpr_SelRy <= opcode(7 downto 5);
-      gpr_SelRx <= opcode(2 downto 0);
-      gpr_SelRi <= opcode(7 downto 5);
+      -- TODO need some way to indicate write complete
+      --gpr_en <= '0';
+      --spr_en <= '0';
 
     -- Branching
     elsif opcode(11 downto 10) = "11" then
-      alu_f <= "0000";
-      gpr_en <= '0';
-      spr_en <= '0';
-      pc_en <= '1';
-      ar_en <= '0';
-      
 
+      v <= "00000000";  -- initialise v
+      
       if opcode(15) = '1' then
         case opcode(14 downto 12) is
           when "000" => -- BEQ
             if spr_Ro(0) = '1' then -- Z=1
-              v := opcode(9 downto 2);
+              v <= opcode(9 downto 2);
             end if;
           when "001" => -- BNE
             if spr_Ro(0) = '0' then -- Z=0
-              v := opcode(9 downto 2);
+              v <= opcode(9 downto 2);
             end if;
           when "010" => -- BLT
             if spr_Ro(0) = '0' and spr_Ro(2) = '1' then -- Z=0 and N=1
-              v := opcode(9 downto 2);
+              v <= opcode(9 downto 2);
             end if;
           when "011" => -- BGT
             if spr_Ro(0) = '0' and spr_Ro(2) = '0' then -- Z=0 and N=0
-              v := opcode(9 downto 2);
+              v <= opcode(9 downto 2);
             end if;
           when "100" => -- BC
             if spr_Ro(1) = '1' then -- C=1
-              v := opcode(9 downto 2);
+              v <= opcode(9 downto 2);
             end if;
           when "101" => -- BNC
             if spr_Ro(1) = '0' then -- C=0
-              v := opcode(9 downto 2);
+              v <= opcode(9 downto 2);
             end if;
           when "110" => -- RJMP
-            v := opcode(9 downto 2);
+            v <= opcode(9 downto 2);
 
           when others =>
-            v := "00000000";
-        end case;
-        
-        -- TODO needs adders
-        --PC_Ri <= PC_Ro + v;
+            v <= "00000000";
+        end case;      
 
-      elsif opcode(15 downto 12) = "0111" then -- JMP
-        -- TODO
+        -- PC <- PC + v
+        -- TODO make lengths compatible - 8 bit and 16 bit
+        AdderCin <= '0';
+        A <= PC_Ro;
+        B <= v;
+        pc_Ri <= Sum;
         
+      elsif opcode(15 downto 12) = "0111" then -- JMP
+        ay <= opcode(6 downto 5);
+
+        -- PC <- ay
+        ar_SelRo <= ay;
+        pc_Ri <= ar_Ro;
+      else
+        pc_Ri <= pc_Ro; -- no change
       end if;
+
+      pc_en <= '1';
+
+      -- TODO need some way to indicate write complete
+      pc_en <= '0';
 
 
     -- Addressing
     else
-      alu_f <= "0000";
-      gpr_en <= '0';
-      spr_en <= '0';
-      pc_en <= '0';
 
       case opcode(12 downto 10) is
 
-        -- TODO : complete addressing
-      
         when "001" => -- Load
-          ar_en <= '0';
+          if opcode(15) = '1' then  -- immediate
+            rx <= 0 & opcode(1 downto 0);
+            v <= opcode(9 downto 2);
 
+            -- rx <- v
+            gpr_SelRi <= rx;
+            gpr_Ri <= v;
+          else                      -- direct
+            rx <= opcode(2 downto 0);
+            ay <= opcode(6 downto 5);
+
+            -- TODO rx <- ay
+          end if;
+
+          gpr_en <= '1';
+          
+          -- TODO need some way to indicate write complete
+          gpr_en <= '0';
+
+
+          case opcode(14 downto 13) is
+            when "01" =>            -- auto increment
+              ay <= opcode(6 downto 5);
+
+              -- TODO increment AR
+
+            when "10" =>            -- auto decrement
+              ay <= opcode(6 downto 5);
+
+              -- TODO decrement AR 
+
+            when others =>
+              -- do nothing
+          end case;
 
         when "101" => -- Store
-          ar_en <= '1';
+          if opcode(15) = '1' then  -- immediate
+            ay <= opcode(1 downto 0);
+            v <= opcode(9 downto 2);
+
+            -- TODO [ay] <- v
+
+          else                      -- direct
+            rx <= opcode(2 downto 0);
+            ay <= opcode(6 downto 5);
+
+            -- TODO [ay] <- rx
+          end if;
+
+          case opcode(14 downto 13) is
+            when "01" =>            -- auto increment
+              ay <= opcode(6 downto 5);
+              -- TODO increment AR
+
+            when "10" =>            -- auto decrement
+              ay <= opcode(6 downto 5);
+              -- TODO decrement AR 
+
+            when others =>
+              -- do nothing
+          end case;
 
 
         when "100" => -- Move
-          ar_en <= '1';
+          if opcode(9) = '1' then       -- MV ay(HL), rx
+
+          elsif opcode(4) = '1' then    -- MV rx, ay(HL)
+
+          else   
+            rx <= opcode(2 downto 0);
+            ry <= opcode(7 downto 5);
+
+            -- ry <- rx
+            gpr_SelRx <= rx;
+            gpr_SelRi <= ry;
+
+            gpr_en <= '1';
+
+            -- TODO need some way to indicate write complete
+            gpr_en <= '0';
+
+          end if;
 
 
         when others =>
           ar_en <= '0';
 
       end case;
+
+      -- TODO need some way to indicate write complete
+      ar_en <= '0';
+
+
     end if;
 
   end process;
